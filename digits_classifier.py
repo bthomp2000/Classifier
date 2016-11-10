2f8b2b05ea91d7f5efa6bc3fd1683196120f4c53
Brian Thompson <brthomp2@illinois.edu>
2016-11-10 01:45:19 -0600

    Added Graphics

--- digits_classifier.py
+++ digits_classifier.py
@@ -1,5 +1,9 @@
 from copy import deepcopy
-K=1.0
+from math import log
+from graphics import *
+import colorsys
+
+K=0.2
 # 0 is white, 1 is gray, 2 is black
 trainingData = list() #[class][row][col][whitePerc,grayPerc,BlackPerc]
 
@@ -7,6 +11,10 @@ classCount = [0] * 10
 traininglabels = []
 testlabels = []
 resultlabels = []
+results = [0]*10
+testcount = [0.0] * 10
+confusion = []
+pairs = []
 def readInput():
 	with open('digitdata/traininglabels') as input_file:
 			for i, line in enumerate(input_file):
@@ -42,7 +50,7 @@ def readTestInput():
 
 	with open('digitdata/testimages') as input_file:
 		imageNum = 0
-		classProb = [1] * 10
+		classProb = [log(0.1)] * 10
 		for intputLineNum, line in enumerate(input_file):
 			currRow = intputLineNum % 28
 			imageNum = int(intputLineNum / 28)
@@ -50,22 +58,15 @@ def readTestInput():
 				feature = line[currCol]
 				for i in range(len(classProb)):
 					if feature == "#":
-						classProb[i]*=trainingData[i][currRow][currCol][0]
+						classProb[i]+=log(trainingData[i][currRow][currCol][0])
 					elif feature == "+":
-						classProb[i]*=trainingData[i][currRow][currCol][1]
+						classProb[i]+=log(trainingData[i][currRow][currCol][1])
 					elif feature == " ":
-						classProb[i]*=trainingData[i][currRow][currCol][2]
+						classProb[i]+=log(trainingData[i][currRow][currCol][2])
 			if currRow == 27:
 				maxIndex = classProb.index(max(classProb))
 				resultlabels.append(int(maxIndex))
 				classProb = [1] * 10
-
-def numCorrect():
-	count = 0.0
-	for i in range(len(resultlabels)):
-		if resultlabels[i] == testlabels[i]:
-			count += 1
-	print count/len(resultlabels)
 
 def initArray():
 	colors = [K] * 3
@@ -85,11 +86,94 @@ def normalize():
 				for l in range(3):
 					trainingData[i][j][k][l]=float(trainingData[i][j][k][l]/((3.0*K)+classCount[i]))
 
+
+def numCorrect():
+	count = 0.0
+	for i in range(10):
+		temp = [0.0] * 10
+		confusion.append(deepcopy(temp))
+	for i in range(len(resultlabels)):
+		testcount[testlabels[i]]+=1.0
+		if resultlabels[i] == testlabels[i]:
+			results[testlabels[i]]+=1.0
+		confusion[testlabels[i]][resultlabels[i]]+=1.0
+
+	for i in range(len(testcount)):
+		results[i]/=testcount[i]
+		for j in range(10):
+			confusion[i][j]/=testcount[i]
+	print results
+	print testcount
+	for i in range(10):
+		for j in range(10):
+			value = confusion[i][j]
+			value=format(value, '2.3f')
+			print value,
+		print ''
+
+def oddsRatios():
+	for k in range(4):
+		temp = [0]*2
+		pairs.append(deepcopy(temp))
+	for c in range(4):
+		maxForThiscurrentc = 0.0
+		for i in range(10):
+			for j in range(10):
+				if confusion[i][j]>maxForThiscurrentc and i != j:
+					maxForThiscurrentc = confusion[i][j]
+					pairs[c][0]=i
+					pairs[c][1]=j
+		confusion[pairs[c][0]][pairs[c][1]]=-10.0
+	print pairs
+
+def printGraphics():
+	for pair in pairs:
+		win = GraphWin("Feature Likelihood for "+str(pair[0])+" and "+str(pair[1]), 900, 300)
+		for i in range(28):
+			for j in range(28):
+				digitClass = pair[0]
+				featurelikelyhood1 = trainingData[digitClass][j][i][0]+trainingData[digitClass][j][i][1]
+				hue = (1-featurelikelyhood1) * 0.67
+				rect = Rectangle(Point(i*10, j*10), Point((i*10)+10, (j*10)+10))
+				color = colorsys.hsv_to_rgb(hue, 1.0, 1.0)
+				color = color_rgb(255*color[0],255*color[1],255*color[2])
+				rect.setFill(color)
+				rect.draw(win)
+
+				digitClass = pair[1]
+				featurelikelyhood2 = trainingData[digitClass][j][i][0]+trainingData[digitClass][j][i][1]
+				hue = (1-featurelikelyhood2) * 0.67
+				rect = Rectangle(Point(i*10+300, j*10), Point((i*10)+10+300, (j*10)+10))
+				color = colorsys.hsv_to_rgb(hue, 1.0, 1.0)
+				color = color_rgb(255*color[0],255*color[1],255*color[2])
+				rect.setFill(color)
+				rect.draw(win)
+
+				odds = log(featurelikelyhood1)-log(featurelikelyhood2)
+				print "odds: ",odds, " = ",featurelikelyhood1,"/",featurelikelyhood2
+				if(odds>5.0):
+					odds=5.0
+				elif(odds<-5.0):
+					odds=-5.0
+				odds = (odds + 5.0)/10.0
+				hue = (1-odds) * 0.67
+				rect = Rectangle(Point(i*10+600, j*10), Point((i*10)+10+600, (j*10)+10))
+				color = colorsys.hsv_to_rgb(hue, 1.0, 1.0)
+				color = color_rgb(255*color[0],255*color[1],255*color[2])
+				rect.setFill(color)
+				rect.draw(win)
+
+	while True:
+		x = 0
+
+
 initArray()
 readInput()
 normalize()
 readTestInput()
 numCorrect()
-# print resultlabels
-# print trainingData[2]
+oddsRatios()
+printGraphics()
+print resultlabels
+print trainingData[2]
 # print classCount
